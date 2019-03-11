d291549a20dd1c87410d4dff673a7331e302f154
wb-tqb429173 <wb-tqb429173@autonavi.com>
2019-03-11 16:42:33 +0800

    delete

--- index.js
+++ index.js
@@ -1,47 +1,46 @@
 let fs = require("fs");
 let path = require("path");
 
-module.exports = function(source, map, meta) {
-  // fs.readFileSync('ccc.js')
-  let query =
-      this.resourceQuery && this.resourceQuery.replace("?", "")
-        ? this.resourceQuery.replace("?", "").split("&")
-        : [],
-    queryObj = {};
-  if (query.length) {
-    query.forEach(item => {
-      let obj = item.split("=");
-      queryObj[obj[0]] = obj[1];
-    });
-  }
-  let reqList = this.request.split("!");
-  let req = reqList[reqList.length - 1];
-  req = req.replace(this.resourceQuery, "");
-  let channel = "";
-  if (queryObj.channel) {
-    channel = queryObj.channel + "\\";
-  }
-  let fileName = null,
-    newFilePath = "",
-    newfileObject = "";
-  (filePathList = req.split("\\")),
-    (fileName = filePathList.pop()),
-    (urlBack = 0);
-  if (queryObj.url) {
-    urlBack = queryObj.url.match(/..\//g)
-      ? queryObj.url.match(/..\//g).length
-      : 0;
-    newfileObject = queryObj.url.replace(/..\//g, "");
-    newfileObject = newfileObject ? newfileObject + "\\" : "";
-  }
-  for (let i = 0; i < filePathList.length - urlBack; i++) {
-    newFilePath += filePathList[i] + "\\";
-  }
-  newFilePath += newfileObject + channel + fileName;
-  let text = fs.readFileSync(newFilePath, "utf-8");
+module.exports = function (source, map, meta) {
+    // fs.readFileSync('ccc.js')
+    let query =
+        this.resourceQuery && this.resourceQuery.replace("?", "") ?
+        this.resourceQuery.replace("?", "").split("&") : [],
+        queryObj = {};
+    if (query.length) {
+        query.forEach(item => {
+            let obj = item.split("=");
+            queryObj[obj[0]] = obj[1];
+        });
+    }
+    let reqList = this.request.split("!");
+    let req = reqList[reqList.length - 1];
+    req = req.replace(this.resourceQuery, "");
+    let channel = "";
+    if (queryObj.channel) {
+        channel = queryObj.channel + "\\";
+    }
+    let fileName = null,
+        newFilePath = "",
+        newfileObject = "",
+        filePathList = req.split("\\"),
+        fileName = filePathList.pop(),
+        urlBack = 0;
+    if (queryObj.url) {
+        urlBack = queryObj.url.match(/..\//g) ?
+            queryObj.url.match(/..\//g).length :
+            0;
+        newfileObject = queryObj.url.replace(/..\//g, "");
+        newfileObject = newfileObject ? newfileObject + "\\" : "";
+    }
+    for (let i = 0; i < filePathList.length - urlBack; i++) {
+        newFilePath += filePathList[i] + "\\";
+    }
+    newFilePath += newfileObject + channel + fileName;
+    let text = fs.readFileSync(newFilePath, "utf-8");
 
-  console.log(`1---------------`);
-  console.log(source);
-  console.log(`1---------------`);
-  return `${text}`;
+    console.log(`1---------------`);
+    console.log(source);
+    console.log(`1---------------`);
+    return `${text}`;
 };
